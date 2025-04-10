e556d742ee661b3f925dfc7efc6f7c11cf7f382d
Alejo Demitropulos <alejodemitropulos@Alejos-MacBook-Pro.local>
2025-04-10 10:58:45 -0300

    uodate lambda permission

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -15,6 +15,11 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_s3" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
